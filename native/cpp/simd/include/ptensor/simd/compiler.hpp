29d35f708c6864534bd5e698d1d49665de54ede8
Otag <otagoam86@gmail.com>
2026-03-06 16:45:45 -0300

    Fix win64 build

--- native/cpp/simd/include/ptensor/simd/compiler.hpp
+++ native/cpp/simd/include/ptensor/simd/compiler.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__clang__)
     #define PTENSOR_AVX2
-#elif defined(__x86_64__) || defined(__i386__)
+#elif defined(__x86_64__) || defined(__i386__) || defined(_M_X64) || defined(_M_IX86)
     #define PTENSOR_AVX2 __attribute__((target("avx2")))
 #else
     #define PTENSOR_AVX2

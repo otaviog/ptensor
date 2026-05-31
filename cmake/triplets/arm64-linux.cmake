f738c2b57b639ea0f7653f0f78c1520505ecd7ef
Otavio Gomes <otagoam86@gmail.com>
2026-05-31 09:47:26 -0300

    Add script to download onnxruntime, and future project stuff

---
+++ cmake/triplets/arm64-linux.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CXX_FLAGS "")
+set(VCPKG_C_FLAGS "-Wno-incompatible-pointer-types-discards-qualifiers")
